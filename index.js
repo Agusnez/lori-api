227f3f477313330b60e1bfa097254a4e34ac59c1
Agustín Núñez <seviagus@gmail.com>
2019-04-21 10:53:14 +0200

    Improvement on the requests

--- index.js
+++ index.js
@@ -3,36 +3,49 @@ const fs = require('fs')
 
 let config = {}
 
-fs.readFile("config.json", {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        config = JSON.parse(data)
-        fs.readFile(config.filepath, {encoding: 'utf-8'}, function(err,alerts){
-            if (!err) {
-                //var newData = alerts
-                request({
-                    url: "http://127.0.0.1:3000/" + config.client_name,
-                    method: "POST",
-                    headers: {
-                        "content-type": "text/plain",  // <--Very important!!!
-                    },
-                    body: alerts
-                }, function (error, response, body){
-                    console.log(response)
-                    if(response.body == 'Log saved!')
-                        fs.writeFile(config.filepath, '', function(){console.log('done')}) //Erases the file content
-                })
-            } else {
-                console.log(err)
-            }
-        })
-    } else {
-        console.log(err)
-    }
-})
+console.log('\x1b[32m%s\x1b[0m', 'Lori API is now Running. Watch log file for verification under logs directory.')
 
+setInterval(function () {
+    fs.readFile("config.json", { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            config = JSON.parse(data)
+            fs.readFile(config.filepath, { encoding: 'utf-8' }, function (err, data) {
+                fs.writeFile(config.filepath, '', () => { fs.close })
+                if (!err && data.length > 0) {
+                    const alerts = data.split('\n').reverse().slice(1).reverse()
+                    var body = ''
+                    for (const a of alerts) {
+                        body += '{"index":{}}\n'
+                        body += a + '\n'
+                    }
+                    body += '\n'
+                    request({
+                        url: "http://" + config.elastic_server_ip + ":9200/lori/" + config.client_name + "/_bulk",
+                        method: "POST",
+                        headers: {
+                            "content-type": "application/x-ndjson",
+                        },
+                        body: body
+                    }, function (error, response) {
+                        resJson = JSON.parse(response.body)
+                        if (resJson.errors == false) {
+                            fs.writeFile(config.filepath, '', () => { fs.close }) //Erases the file content
+                            fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Pushed new alerts to ELK\n', () => { fs.close })
+                        } else {
+                            console.log('\x1b[31m%s\x1b[0m', 'An error occurred during updating alerts in Elastic. Please check the logs logs/lori.log')
+                            fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Error: ' + response.body + '\n', () => { fs.close })
+                        }
+                    })
+                } else {
+                    if (err)
+                        fs.writeFile(config.log_filepath, err, () => { })
+                        fs.writeFile(config.filepath, data, () => { fs.close })
 
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
+                    fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Nothing to push to ELK\n', () => { fs.close })
+                }
+            })
+        } else {
+            console.log(err)
+        }
     })
-}
+}, 1000)
