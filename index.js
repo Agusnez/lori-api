133b0bc491402d00f1232725f1fc35201e62412f
Agustín Núñez <seviagus@gmail.com>
2019-04-30 23:44:43 +0200

    Removes uncaught exception

--- index.js
+++ index.js
@@ -20,20 +20,25 @@ setInterval(function () {
                     }
                     body += '\n'
                     request({
-                        url: "http://" + config.elastic_server_ip + ":9200/lori/" + config.client_name + "/_bulk",
+                        url: "https://" + config.elastic_server_ip + ":9200/lori/" + config.client_name + "/_bulk",
                         method: "POST",
                         headers: {
                             "content-type": "application/x-ndjson",
                         },
                         body: body
                     }, function (error, response) {
-                        resJson = JSON.parse(response.body)
-                        if (resJson.errors == false) {
-                            fs.writeFile(config.filepath, '', () => { fs.close }) //Erases the file content
-                            fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Pushed new alerts to ELK\n', () => { fs.close })
+                        if (response) {
+                            resJson = JSON.parse(response.body)
+                            if (resJson.errors == false) {
+                                fs.writeFile(config.filepath, '', () => { fs.close }) //Erases the file content
+                                fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Pushed new alerts to ELK\n', () => { fs.close })
+                            } else {
+                                console.log('\x1b[31m%s\x1b[0m', 'An error occurred during updating alerts in Elastic. Please check the logs logs/lori.log')
+                                fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Error: ' + response.body + '\n', () => { fs.close })
+                            }
                         } else {
-                            console.log('\x1b[31m%s\x1b[0m', 'An error occurred during updating alerts in Elastic. Please check the logs logs/lori.log')
-                            fs.appendFile(config.log_filepath, (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + ' Error: ' + response.body + '\n', () => { fs.close })
+                            fs.writeFile(config.filepath, data, () => { fs.close })
+                            console.log('\x1b[31m%s\x1b[0m', 'An error occurred during updating alerts in Elastic.')
                         }
                     })
                 } else {
